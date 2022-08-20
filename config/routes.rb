86b0e1ea9b6889a1957602fc6f45ef43fd228850
alijm0806 <alijm1104@gmail.com>
2022-08-20 13:32:19 -0500

    Add Movie model and movies controller with index-show routes and actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get "actors" => "actors#index"
   get "actors/:id" => "actors#show"
   post "actors" => "actors#create"
+
+  get "movies" => "movies#index"
+  get "movies/:id" => "movies#show"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-actor.save
+# actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+# actor.save
+
+movie = Movie.new(
+  title: "Titanic",
+  year: 1997,
+  plot: "James Cameron's `Titanic` is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic",
+)
+movie.save
+
+movie = Movie.new(
+  title: "Out of Time",
+  year: 2003,
+  plot: "Matt Lee Whitlock is the chief of police in a small town in Florida called Banyan Key, respected by his peers and loved by his community. But when the town is shocked by a double homicide, everything Matt thought he knew starts to unravel.",
+)
+movie.save
