362c38cac734d6b2abac80c29b4d2d6029c262ab
alijm0806 <alijm1104@gmail.com>
2022-08-28 12:28:36 -0500

    Add validations to movie-app

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,7 @@
 class Actor < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :known_for, inclusion: { in: %w(movie show),
+                                     message: "%{value} is not a valid known_for" }
+  validates :age, comparison: { greater_than: 13 }
 end
